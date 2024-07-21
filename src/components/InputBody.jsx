d621fb8d9eb33b78bd64e93d0d1ac918550f510b
Ujjwaldeep Singh <ujjwal2699singh@gmail.com>
2024-07-21 15:12:08 +0000

    Got jsonpath evaluator working

--- src/components/InputBody.jsx
+++ src/components/InputBody.jsx
@@ -1,37 +1,34 @@
-"use client"
-import * as React from "react"
-import { Label } from "@/components/ui/label"
+import * as React from "react";
 import JsonInput from "./JsonInput";
-import {QueryContext} from '../App'
+import { QueryContext } from '../App';
 import { JSONPath } from "jsonpath-plus";
 
 const InputBody = () => {
-    const {inputText} = React.useContext(QueryContext)
+    const {inputText} = React.useContext(QueryContext);
     const [jsonInputText, setJsonInputText] = React.useState();
     const [jsonOutputText, setJsonOutputText] = React.useState();
- 
+
     React.useEffect(() => {
         try {
-            let result = JSONPath({path: inputText, json: jsonInputText});
-            console.log(result);
-            setJsonOutputText(result);
+            const parsedJson = JSON.parse(jsonInputText);
+            const result = JSONPath({path: inputText, json: parsedJson});
+            setJsonOutputText(JSON.stringify(result, null, 4));
         } catch (e) {
-            console.log(e);
+            console.error(e);
+            setJsonOutputText('Invalid Json Input');
         }
-    })
+    }, [jsonInputText, inputText]);
 
     return (
         <div className='flex flex-row p-6 pt-0'>
             <div className="flex flex-col grow">
-                <Label className="mb-3 text-3xl">Input</Label>
-                <JsonInput value={jsonInputText} onChange={setJsonInputText}/>
+                <JsonInput title="Input" value={jsonInputText} onChange={setJsonInputText} />
             </div>
             <div className="flex flex-col grow ml-6">
-                <Label className="mb-3 text-3xl">Output</Label>
-                <JsonInput value={jsonOutputText} readOnly={true}/>
+                <JsonInput title="Output" value={jsonOutputText} readOnly={true}/>
             </div>
         </div>
     )
 }
 
-export default InputBody
+export default InputBody;
--- src/components/JsonInput.jsx
+++ src/components/JsonInput.jsx
@@ -1,22 +1,39 @@
-"use client"
-import * as React from "react"
+import * as React from "react";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch"
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/theme-twilight";
 import "ace-builds/src-noconflict/mode-json";
-import "ace-builds/src-noconflict/ext-language_tools"
+import "ace-builds/src-noconflict/ext-language_tools";
 
 const JsonInput = (props) => {
-    const { readOnly, value, onChange } = props;
+  const { title, readOnly, value, onChange } = props;
 
-    return <AceEditor
+  return <>
+    <div className="p-0 m-0 flex flex-row justify-b">
+        <Label className="mb-3 text-3xl">{title}</Label>
+        { readOnly && <div className="flex items-center space-x-2">
+            <Switch id="airplane-mode" />
+            <Label htmlFor="airplane-mode">Remove special characters</Label>
+        </div>}
+    </div>
+    <AceEditor
         mode="json"
         theme='twilight'
         height='700px'
         width="auto"
+        name="editor"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true
+        }}
         readOnly={readOnly}
         value={value}
         onChange={onChange}
     />
-}
+  </>
+};
 
-export default JsonInput
+export default JsonInput;
