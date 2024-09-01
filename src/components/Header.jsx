a9569a3ad580455003d8b635725b20e7c6413da0
Ujjwaldeep Singh <ujjwal2699singh@gmail.com>
2024-09-01 12:09:28 +0000

    Added settings button and minor cosmetic changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,7 @@ import {
 import { GoChevronDown } from "react-icons/go";
 
 import {QueryContext} from '../App'
+import { LuSettings } from "react-icons/lu";
 
 const Header = () => {
     const {inputText, queryLanguage, setInputText, setQueryLanguage} = React.useContext(QueryContext);
@@ -24,13 +25,19 @@ const Header = () => {
 
     return (
         <div className='flex flex-col p-6'>
-            <p className='scroll-m-20 text-3xl font-extrabold tracking-tight first:mt-0 pb-6'>
-                Welcome to Jsonpath Evaluator
-            </p>
+            <div className='flex flex-row justify-between align-items-center w-full'> 
+                <p className='scroll-m-20 text-2xl font-extrabold tracking-tight first:mt-0 pb-6'>
+                    JSONpath Evaluator
+                </p>
+                <Button size="icon">
+                    <LuSettings className='text-2xl m-0 p-0'/>
+                </Button>
+            </div>
+            
             <div className='flex flex-row'>
                 <DropdownMenu>
                     <DropdownMenuTrigger className='mr-6'>
-                        <Button variant="ghost" className={queryLanguage === 'JSONPath' ? jsonPathtextColor : jmesPathtextColor}>
+                        <Button variant="secondary" lassName={queryLanguage === 'JSONPath' ? jsonPathtextColor : jmesPathtextColor}>
                             {queryLanguage}
                             <GoChevronDown className='ml-2'/>
                         </Button>
